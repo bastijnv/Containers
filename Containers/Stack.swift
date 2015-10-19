16a22fdf81af6664d25ca220b3cfd30d7a8d5d7f
Bastijn Vissers <bastijn.vissers@philips.com>
2015-10-19 18:01:04 +0200

    Empty & Push v1
    Signed-off-by: Bastijn Vissers <bastijn.vissers@philips.com>

--- Containers/Stack.swift
+++ Containers/Stack.swift
@@ -2,8 +2,18 @@ import Foundation
 
 public class Stack {
 
+    var empty = true;
+    
     public init() {
 
     }
 
+    public func isEmpty() -> Bool {
+        return empty;
+    }
+    
+    public func push(id: Int) -> Stack{
+        empty = false;
+        return self;
+    }
 }
--- ContainersTests/StackTests.swift
+++ ContainersTests/StackTests.swift
@@ -7,11 +7,38 @@ class StackTests: QuickSpec {
     override func spec() {
 
         describe("stack") {
-
-            it("can be created") {
+            it("Start be created") {
                 expect(Stack()).toNot(beNil())
             }
-
+            
+            it("Can be checked for emptyness"){
+                expect(Stack().isEmpty()).to(beTrue());
+            }
+            
+//            it("Can pop"){
+//                var stack = Stack();
+//                
+//            }
+            
+            it("Can push"){
+                var stack = Stack();
+                expect(stack.push(1).isEmpty()).to(beFalse());
+                
+            }
+        }
+        
+        describe("Crane"){
+            it("Can raise a container on top of the stack"){
+                
+            }
+            
+            it("Can lower a container from the top of the stack"){
+                
+            }
+            
+            it("Can inspect whether the stack is empty"){
+                
+            }
         }
     }
 }
